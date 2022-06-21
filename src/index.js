13e30ab3bf5c2510a9ae2ce6912fb4ef0ab2165b
Gabriel <gopxfs@hotmail.com>
2022-06-21 17:06:35 -0300

    Update populateShows
    Now the function wait for all the promises together, instead of following a queue

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import './assets/index.css';
 import TVMaze from './modules/TVMazeAPI.js';
-import Involvement from './modules/InvolvementAPI';
+import Involvement from './modules/InvolvementAPI.js';
 
-const tvMaze = new TVMaze ();
+const tvMaze = new TVMaze();
 const involvement = new Involvement();
 
 const getLikes = async (id) => {
-const likesData = await involvement.getLikes();
-const show = likesData.find(show => show.item_id === id);
-return show.likes;
+  const likesData = await involvement.getLikes();
+  const show = likesData.find((show) => show.item_id === id);
+  return show.likes;
 };
 
-const newShow = async (url, likes) => {
-  tvMaze.createShowLi(await tvMaze.getShowInfo(url), likes);
-  };
+const newShow = async (url, likes) => tvMaze.createShowLi(await tvMaze.getShowInfo(url), likes);
 
-const populateShows = async() => {
+const populateShows = async () => {
+  const likes = [];
   for (let i = 1; i <= 6; i += 1) {
-    await newShow(`https://api.tvmaze.com/shows/${i}`, await getLikes(`${i}`));
+    likes.push(getLikes(`${i}`));
   }
-}
+  Promise.all(likes)
+    .then((results) => {
+      for (let i = 1; i <= 6; i += 1) {
+        newShow(`https://api.tvmaze.com/shows/${i}`, results[i - 1]);
+      }
+    });
+};
 populateShows();
--- src/modules/InvolvementAPI.js
+++ src/modules/InvolvementAPI.js
@@ -1,31 +1,31 @@
 class Involvement {
   constructor() {
-   this.baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
-   this.appID = 'wWiBGZy0Ro0ezfkPSzh8';
+    this.baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
+    this.appID = 'wWiBGZy0Ro0ezfkPSzh8';
   }
 
   addLike = async (id) => {
-      await fetch (`${this.baseURL}/apps/${this.appID}/likes`, {
+    await fetch(`${this.baseURL}/apps/${this.appID}/likes`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        "item_id": `${id}`,
+        item_id: `${id}`,
       }),
     });
   };
 
   getLikes = async () => {
-   const response = await fetch (`${this.baseURL}/apps/${this.appID}/likes`)
-   .then(response => response.json())
-   return response;
+    const response = await fetch(`${this.baseURL}/apps/${this.appID}/likes`)
+      .then((response) => response.json());
+    return response;
   };
 
   createApp = async () => {
     await fetch(`${this.baseURL}/apps/`, {
       method: 'POST',
-    })
+    });
   };
 }
 
