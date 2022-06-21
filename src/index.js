ee99170929b5552e596ab2d1873362979bc307b0
Gabriel <gopxfs@hotmail.com>
2022-06-21 02:48:19 -0300

    Add shows interface
    Add show interface containing:
    Image, Title, like button, like quantity, comment button

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import TVMaze from './modules/TVMazeAPI.js';
 
 const tvMaze = new TVMaze ();
 
+const newShow = async (url) => {
+tvMaze.createShowLi(await tvMaze.getShowInfo(url));
+};
+for (let i = 0; i <= 6; i += 1) {
+  newShow(`https://api.tvmaze.com/shows/${i}`)
+}
 
--- src/modules/TVMazeAPI.js
+++ src/modules/TVMazeAPI.js
@@ -1,9 +1,28 @@
-class TVMaze {
+class TVMaze { 
+  createShowLi = (show) => {
+    const homepageUl = document.getElementById('showList')
+    const showLi = document.createElement('li');
+    const showImg = document.createElement('img');
+    const showTitle = document.createElement('h2');
+    const showLike = document.createElement('input');
+    const showLikes = document.createElement('p');
+    const showComments = document.createElement('button');
+    showImg.setAttribute('alt', 'Show thumbnail');
+    showLike.setAttribute('type', 'checkbox');
+    showImg.setAttribute('src', show.image.original);
+    showImg.setAttribute('height', 200);
+    showTitle.innerHTML = show.name;
+    showComments.innerHTML = 'Comments';
+    showLi.append(showImg,showTitle,showLike,showLikes,showComments);
+    homepageUl.append(showLi);
+    return showLi;
+  }
+
   getShowInfo = async (url) => {
     let show;
     await fetch(url)
+    .then(response => response.json())
     .then((response => {show  = response}));
-    console.log(show);
     return show;
   };
 }
